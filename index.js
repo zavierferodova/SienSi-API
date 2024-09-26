ede3d0d5949d6320a4bbe07574d753ed710f1b21
Zavier Ferodova <zavierferodova@gmail.com>
2024-09-26 17:08:46 +0700

    Fix CORS

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const helmet = require('helmet')
 const app = express()
-const port = 3000
+const port = 8000
 const routes = require('./src/routes')
-const path = require('path')
+const cors = require('cors')
 
-app.set('views', path.join(__dirname, 'resources/views'))
-app.set('view engine', 'ejs')
-
+app.use(cors())
 app.use(express.static('public'))
 app.use(helmet())
 app.use(express.json())
